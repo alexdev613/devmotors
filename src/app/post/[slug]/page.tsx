b64269da73dc6d16868f505690d48dc7c1e8c585
Alex Nascimento <alexjose68@hotmail.com>
2024-08-06 17:42:45 -0300

    feat: montamos a página recebendo em cada elemento as informações pertinentes para deixar a página intuitiva e apresentável, reutilizamos componentes já construídos como o Container e o Hero, e também utilizamos uma Image e já passando a propriedade sizes, que é uma das exigências pra quando utilizamos a propriedade fill, e para tirar o aviso que tinhamos no console do navegador, passamos a propriedade sizes com seu argumento.

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,6 +1,10 @@
+import { Container } from '@/components/container';
 import styles from './styles.module.scss';
+import { Hero } from '@/components/hero';
 import { getItemBySlug } from '@/utils/actions/get-data';
 import { PostProps } from '@/utils/post.type';
+import { Phone } from 'lucide-react';
+import Image  from 'next/image'
 
 export default async function Page({ params: { slug } }: { params: { slug: string } }) {
 
@@ -8,8 +12,49 @@ export default async function Page({ params: { slug } }: { params: { slug: strin
   console.log(JSON.stringify(objects, null, 2));
 
   return (
-    <div>
-      <h1>Página de {slug}</h1>
-    </div>
+    <>
+      <Hero
+        heading={objects[0].title}
+        buttonTitle={objects[0].metadata.button.title}
+        buttonUrl={objects[0].metadata.button.url}
+        bannerUrl={objects[0].metadata.banner.url}
+        icon={<Phone size={24} color="#FFF" />}
+      />
+
+      <Container>
+        <section className={styles.about}>
+          <article className={styles.innerAbout}>
+            <h1 className={styles.title}>
+              {objects[0].metadata.description.title}
+            </h1>
+            <p>
+              {objects[0].metadata.description.text}
+            </p>
+
+            {objects[0].metadata.description.button_active && (
+              <a
+                href={objects[0].metadata.description.button_url as string}
+                target='_blank'
+                className={styles.link}
+              >
+                {objects[0].metadata.description.button_title}
+              </a>
+            )}
+          </article>
+
+          <div className={styles.bannerAbout}>
+            <Image
+              className={styles.imageAbout}
+              src={objects[0].metadata.description.banner.url}
+              alt={objects[0].title}
+              quality={100}
+              fill={true}
+              priority={true}
+              sizes="(max-width: 480px) 100vw, (max-width: 1024px) 75vw, 60vw"
+            />
+          </div>
+        </section>
+      </Container>
+    </>
   )
 }
