d7a76e012de0e4592b29d448061760c7c4f87257
Alex Nascimento <alexjose68@hotmail.com>
2024-08-07 21:51:40 -0300

    feat: importei o redirect do next/navigation para redirecionar o usuário para a página Home para quando ele acessar uma página inexistente ser redirecionado instantaneamente, sem nem passar para a página de Erro! É uma boa, mas preferi comentar esse acréscimo deixando a página de erro ativa.

--- src/utils/actions/get-data.ts
+++ src/utils/actions/get-data.ts
@@ -1,3 +1,5 @@
+// import { redirect } from "next/navigation";
+
 export async function getDataHome() {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects/66abddd3d70e5c5cd3fa6317?read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata,`, { next: { revalidate: 120 }},);
@@ -16,7 +18,7 @@ export async function getDataHome() {
 export async function getSubMenu() {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/objects?pretty=true&query=%7B%22type%22:%22pages%22%7D&limit=10&read_key=${process.env.READ_KEY}&depth=1&props=slug,title,`, { next: { revalidate: 120 } });
-  
+
     if(!res.ok) {
       throw new Error("Failed to fetch menu data");
     }
@@ -52,6 +54,7 @@ export async function getItemBySlug(itemSlug: string) {
     return res.json();
 
   } catch (err) {
+    // redirect("/");
     throw new Error("Failed get item by slug");
   }
 }
